fac3932a567a7a1fe1f4a6ae3936d536a3f1d749
William West <wwest466@gmail.com>
2020-08-17 19:24:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,3 +10,16 @@ end
 return "HAPPY NEW YEAR!"
   
 end
+
+def countdown_with_sleep(number)
+
+until number == 0 do
+  puts "#{number} SECOND(S)!"
+  number -= 1
+  sleep(1)
+end
+
+return "HAPPY NEW YEAR!"
+  
+end
+
